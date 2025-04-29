8ca742f369974afa201ec8c6b2299e024271ac51
Devin Naicker <thirushandevinnaicker@gmail.com>
2025-04-29 18:56:21 +0200

    3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Module that defines a regular function to create an asyncio Task for wait_random.
+Module that defines a regular function to create an asyncio Task for
+wait_random.
 """
 
 import asyncio
