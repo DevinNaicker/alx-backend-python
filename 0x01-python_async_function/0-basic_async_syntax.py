42d964e672a301b5a15cc19de7a938c41d2cfb20
Devin Naicker <thirushandevinnaicker@gmail.com>
2025-04-29 17:57:27 +0200

    0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-"""
-This module defines an asynchronous coroutine that waits for a random delay 
-and returns the delay time.
+"""This module defines an asynchronous coroutine that waits for a random 
+delay and returns the delay time.
 """
 
 
