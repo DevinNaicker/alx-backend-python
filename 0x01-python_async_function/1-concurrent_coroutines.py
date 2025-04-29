baa2384b296d4f82ba90668f25e13ee038624164
Devin Naicker <thirushandevinnaicker@gmail.com>
2025-04-29 18:18:29 +0200

    1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Module that defines an async routine to spawn multiple wait_random calls concurrently.
+Module that defines an async routine to spawn multiple wait_random calls
+concurrently.
 """
 
 import asyncio
@@ -8,7 +9,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
-    """Spawn wait_random n times with the specified max_delay and return delays in ascending order."""
+    """Spawn wait_random n times with the specified max_delay and return
+    delays in ascending order."""
     delays = []
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
 
